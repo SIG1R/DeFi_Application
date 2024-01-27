7f11e6f2fce1adf0c8340f5d36dc76242dd4bb84
SIG1R <grsebas04@gmail.com>
2024-01-27 12:31:35 -0500

    zero-coupon

--- app.py
+++ app.py
@@ -13,15 +13,16 @@ type_bond = st.selectbox(
 )
 
 # Setting dates about the bond
-issue_date, expiration_date = st.columns(2)
+ISSUE_DATE, EXPIRATION_DATE = st.columns(2)
 
-with issue_date: # Set issue_date input
-    fecha_inicio = st.date_input('Fecha de emisión')
+with ISSUE_DATE: # Set issue_date input
+    issue_date = st.date_input('Fecha de emisión')
 
-with expiration_date: # Set expiration_date input
-    fecha_fin = st.date_input('Fecha de vencimiento')
+with EXPIRATION_DATE: # Set expiration_date input
+    expiration_date = st.date_input('Fecha de vencimiento')
+
 
 #st.write(type(fecha_fin))
 if st.button('calcular'):
     
-    st.write((fecha_fin-fecha_inicio).days)
+    st.write(val.zero_coupon(0.1, expiration_date, issue_date))
--- valuation.py
+++ valuation.py
@@ -1,16 +1,18 @@
 import numpy as np
+import datetime as dt
 
-def zero_coupon(rate, time, duration):
+def zero_coupon(rate, expiration_date, issue_date = dt.date.today()):
     """
     rate -> tasa de interés (r)
     time -> tiempo que ha pasado
     total_duration -> tiempo de duración del bono
     """
-    assert time <= duration, 'La duración del bono no puede ser menor que el tiempo transcurrido'
+    assert issue_date <= expiration_date, 'La duración del bono no puede ser menor que el tiempo transcurrido'
 
-    time_diff = duration - time
+    time_diff = expiration_date - issue_date
 
-    return np.e**(-rate*time_diff)
+    return np.e**(-rate*time_diff.days)
+
 
 
 
