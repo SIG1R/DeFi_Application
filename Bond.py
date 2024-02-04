46eb006e171572762b846d2dc51e877afa0e5dff
SIG1R <grsebas04@gmail.com>
2024-02-04 14:00:09 -0500

    Backup

--- Bond.py
+++ Bond.py
@@ -3,81 +3,131 @@ import datetime as dt
 
 class Bond:
 
-    def __init__(self, type_bond,
-                 date_issue, date_expiration,
-                 face_value,market_value):
-        
-        assert date_issue < date_expiration, 'La fecha de emisión del bono no puede ser mayor que la fecha de vencimiento'
+    def __init__(self, type_bond = 'Con Cupon',
+                 issue_date = dt.date.today(), expiration_date = dt.date.today(),
+                 face_rate = 0, market_value = 0):
+        """
+        Inicializador de la clase bonos, establece variables de instancia características
+        de un bono las cuales son:
+        - type_bond             establece el tipo de bono (con cupón o sin cupón)
+        - face_rate
+        - market_value          indica la tasa de interés a la que se negocia el bono en la actualidad
+        - issue_date            referencia la fecha en la cual el bono fue emitidio
+        - expiration_date       referencia la fecha de vencimiento del bono
+        """
+
+        # Asegurarse de que las fechas no tengan problemas temporales
+        assert issue_date <= expiration_date, 'La fecha de emisión del bono no puede ser mayor que la fecha de vencimiento'
 
         self.type_bond = type_bond # Tipo de bono (con o sin cupón)
         
-        # Valores o tasas de interés del bono
-        self.face_value = face_value        # Cuando fue emitido
-        self.market_value = market_value    # Cuando se toma la medición
-        self.daily_value = 0                # Conversión a tasa diaria
+        # Almacenar las variables de instancia sobre las tasas de interés del bono
+        self.face_rate = face_rate
+        self.market_value = market_value
 
-        # Fechas correspondientes a
-        self.date_issue = date_issue            # Cuando fue emitido
-        self.date_expiration = date_expiration  # Cuando vence
+        # Almacenar las variables de instancia sobre las fechas del bono
+        self.issue_date = issue_date
+        self.expiration_date = expiration_date
 
-        self.valuation_now = 0
-    
-    def get_coupon_dates(self):
-        TODAY = dt.date.today()
-        dates = [dt.date(TODAY.year+i,self.date_expiration.month,self.date_expiration.day) for i in range(1+self.date_expiration.year - TODAY.year)]
-        if dates[0] < TODAY: del dates[0]
-        self.coupon_dates = dates
+        self.get_payments_dates()
+        self.get_daily_rate()
+        self.get_cash_flow()
 
 
-    def daily_rate(self):
+    def update(self, dataframe):
         """
-        Convierte una tasa de interés anual en diaria
+        Método que permite la actualización de un bono, es decir, al mínimo
+        cambio en alguna variable de instacia sobre la instancia del bono
+        se re-ejecutan los métodos asociados al bono y por ende, se
+        re-asignan las variables de instancia
         """
-        daily_fract = 1/365
-        result = (1+self.market_value/100)**daily_fract
-        result = 100*(result-1)
-        result = round(result, 3)
-        self.daily_value = result
+        
+        self.get_valuation()
+        self.get_duration(dataframe)
+        self.get_convexity(dataframe)
 
-    def cash_flows(self):
+        self.get_daily_rate()
+        self.get_payments_dates()
+        self.get_cash_flow()
+
+    def get_payments_dates(self):
+        """
+        Método que cálcula las siguientes fechas de cobro del bono y lo almacena en una
+        variable de instancia
+        """
+
+        TODAY_ = dt.date.today()
+
+
+        payments_dates = [
+                dt.date(TODAY_.year+i,self.expiration_date.month,self.expiration_date.day) # Las fechas de cobro son anuales --> ni los meses ni días varian
+                for i in range(1+self.expiration_date.year - TODAY_.year) # Se obtienen la cantidad de años de diferencia y se le suma 1 
+                ]                                                         # para que el año de vencimiento se incluya en el ciclo for
+
+        if payments_dates[0] < TODAY_: del payments_date[0]  # Se elimina la primera fecha en caso de que ya se haya reclamado el cupón
+        
+        self.payments_dates = payments_dates # Se almacenan las fechas en una variable de instancia
+        self.total_payments = len(payments_dates)
+
+
+    def get_daily_rate(self):
+        """
+        Método que permite la conversión de tasas de interés del bono anual a diario
+        """
+
+        daily_fract = 1/365                         # Razón sobre la que se efectuará el cambio (diario)
+        r = (1+self.market_value/100)**daily_fract  # Se efectua la conversión de tasas
+        r = 100*(r-1)                          # Convierte en valor porcentual
+        r = round(r, 3)                        # Se redondea la tasa a tres decimales
+        self.daily_rate = r                        # Se almacena en una variable de instancia
+
+    def get_cash_flow(self):
+        """
+        Método que cálcula el flujo de caja del bono
+        """
 
         # ----- Getting cash flow -----
-        cash_flow_ = self.face_value * np.ones(len(self.coupon_dates)-1)
-        self.cash_flow_ = np.append(cash_flow_, self.face_value + 100)
+        cash_flow_ = self.face_rate * np.ones(len(self.payments_dates)-1)
+        self.cash_flow_ = np.append(cash_flow_, self.face_rate + 100)
 
         # ----- Getting cash flow in present value -----
         present_cash_flow = np.array([])
-        base = 1+self.daily_value/100
+        base = 1+self.daily_rate/100
         for index,element in enumerate(self.cash_flow_):
-            aux_ = base**((self.coupon_dates[index] - self.date_issue).days)
+            aux_ = base**((self.payments_dates[index] - self.issue_date).days)
             aux_ = self.cash_flow_[index]/aux_
             present_cash_flow = np.append(present_cash_flow, round(aux_,3))
 
         self.present_cash_flow = present_cash_flow
 
     
-    def valuation(self):
-        
-        power = (self.date_expiration - self.date_issue).days
+    def get_valuation(self):
+        """
+        Método que cálcula la valuación de un bono dependiendo del tipo de
+        bono (con cupón o sin cupón)
+        """
+
+        power = (self.expiration_date - self.issue_date).days # Para cualquiera de los 2 tipos de bonos, la potencia de la fórmula es la mísma
 
         if self.type_bond == 'Zero Cupon': # Sí el bono es de tipo zero-coupon
-            self.valuation_now = np.e**(-self.daily_value/100*power)
+            self.valuation = np.e**(-self.daily_rate/100*power)
 
         else: # Sí el bono es de tipo con cupón
             rate = self.market_value/100
-            basic_form = 1+rate**((self.date_expiration - self.date_issue).days)
+            basic_form = 1+rate**((self.expiration_date - self.issue_date).days)
             
-            left = self.face_value*(basic_form -1)/(basic_form*rate)
+            left = self.face_rate*(basic_form -1)/(basic_form*rate)
             right = 100/basic_form
 
-            self.valuation_now = left + right
+            self.valuation = left + right
 
-    def duration(self, dataframe):
-        self.duration_ = sum(dataframe['N° pago * Valor presente FC'])/sum(dataframe['Valor presente FC'])
+    def get_duration(self, dataframe):
+        self.duration = sum(dataframe['N° pago * Valor presente FC'])/sum(dataframe['Valor presente FC'])
 
-    def convexity(self, dataframe):
+    def get_convexity(self, dataframe):
         aux = sum(dataframe['N° pago^2 * Valor presente FC'])/sum(dataframe['N° pago * Valor presente FC'])
-        self.convexity_ = round(aux/sum(self.cash_flow_),3)
+        self.convexity = round(aux/sum(self.cash_flow_),3)
 
     def change_price(self, basic_points):
-        return self.duration_*basic_points + 0.5*self.convexity_*basic_points**2
+        
+        return self.duration*basic_points + 0.5*self.convexity*basic_points**2    
--- app.py
+++ app.py
@@ -2,18 +2,18 @@ import streamlit as st
 from Bond import *
 import pandas as pd
 
-#import numpy as np
-#import matplotlib.pyplot as plt
 
 st.write("""
 # Bond calculator
 Establecer características del bono
 """)
 
+bond = Bond()
+
 # Setting type of bond
 type_bond = st.radio(
     'Tipo de bono',
-    ['Zero Cupon', 'Con Cupon']
+    ['Con Cupon', 'Zero Cupon']
 )
 
 
@@ -21,14 +21,14 @@ type_bond = st.radio(
 ISSUE_DATE, EXPIRATION_DATE = st.columns(2)
 
 with ISSUE_DATE: # Set issue_date input
-    date_issue = st.date_input(
+    issue_date = st.date_input(
             'Fecha de emisión',
             format='DD/MM/YYYY',
-            value=dt.date(2024,1,25)
+            value=dt.date(2024,1,25),
     )
 
 with EXPIRATION_DATE: # Set expiration_date input
-    date_expiration= st.date_input(
+    expiration_date = st.date_input(
             'Fecha de vencimiento',
             format='DD/MM/YYYY',
             value=dt.date(2027,11,3)
@@ -39,51 +39,37 @@ with EXPIRATION_DATE: # Set expiration_date input
 RATE_ISSUE_COLUMN, RATE_MARKET_COLUMN = st.columns(2)
 
 with RATE_ISSUE_COLUMN: # Set issue_rate input
-    face_value = st.number_input('Tasa facial (%)', value=5.75)
+    face_rate = st.number_input('Tasa facial (%)', value=5.75)
 
 with RATE_MARKET_COLUMN: # Set actual_date input
-    market_value = st.number_input('Tasa de mercado (%)', value=10.19)
+    market_rate = st.number_input('Tasa de mercado (%)', value=10.19)
 
 bond = Bond(
         type_bond,
-        date_issue, date_expiration,
-        face_value, market_value)
-
-st.write(f'El cupón es de tipo {bond.type_bond}')
-st.write(f'El cupón es fue emitido en la fehca {bond.date_issue}')
-st.write(f'El cupón se vence en la fehca {bond.date_expiration}')
-st.write(f'El cupón tiene una tasa facial de {bond.face_value}')
-st.write(f'El cupón tiene una tasa anual en el mercado de {bond.market_value}')
-bond.daily_rate()
-st.write(f'El cupón tiene una tasa diaria en el mercado de {bond.daily_value}')
-bond.valuation()
-st.write(f'El bono está valorado en {bond.valuation_now}')
-
-bond.get_coupon_dates()
-bond.cash_flows()
-
-payments = [i+1 for i in range(len(bond.coupon_dates))]
+        issue_date, expiration_date,
+        face_rate, market_rate)
 
 data_example = pd.DataFrame({
-    'N° pago': payments,
-    'Fecha': bond.coupon_dates,
+    'N° pago': [i for i  in range(1,bond.total_payments+1)],
+    'Fecha': bond.payments_dates,
     'FC': bond.cash_flow_,
     'Valor presente FC': bond.present_cash_flow
 })
+
+
 
 data_example['N° pago * Valor presente FC'] =  data_example['N° pago']*data_example['Valor presente FC']
 
 data_example['N° pago^2 * Valor presente FC'] =  data_example['N° pago']**2*data_example['Valor presente FC']
 
+bond.update(data_example)
 st.write(data_example)
 
-bond.duration(data_example)
 
-st.write(f'La duración del bono es {bond.duration_} años')
+st.write(f'La duración del bono es {bond.duration} años')
 
-bond.convexity(data_example)
 
-st.write(f'La convexidad del bono es {bond.convexity_}')
+st.write(f'La convexidad del bono es {bond.convexity}')
 
 
 basic_points = np.linspace(-100,100, 100)
